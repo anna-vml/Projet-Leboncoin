6e837aaf1fd00dda8902ed125b08e0a7746c6a48
rayanelarfi900-wq <rayanelarfi900@gmail.com>
2026-06-20 00:30:47 +0100

    Update sql_annonces.sql

--- sql_annonces.sql
+++ sql_annonces.sql
@@ -1,9 +1,14 @@
 CREATE TABLE annonces (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    utilisateur_id INT NOT NULL,
     titre VARCHAR(255) NOT NULL,
     prix DECIMAL(10,2) NOT NULL,
     etat VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
     image VARCHAR(255),
-    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (utilisateur_id)
+    REFERENCES utilisateurs(id)
+    ON DELETE CASCADE
 );
