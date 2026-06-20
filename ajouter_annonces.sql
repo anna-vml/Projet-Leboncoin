3f3ee05f0684009fe31a3a07590477d5f775ba52
mellitaroudjit24 <melli.taroudjit24@gmail.com>
2026-06-20 17:09:00 -0400

    Refactor SQL insert statements for annonces

--- ajouter_annonces.sql
+++ ajouter_annonces.sql
@@ -1,106 +1,101 @@
-INSERT INTO annonces
-(utilisateur_id, titre, prix, etat, description, image)
-VALUES
-
-(1,'Calculatrice Casio Graph 35+',35,'Bon état',
+INSERT INTO annonces (titre, prix, etat, description, image) VALUES
+ ('Calculatrice Casio Graph 35+',35,'Bon état',
 'Calculatrice graphique idéale pour les étudiants en informatique, mathématiques et économie.',
 'calculatrice.jpg'),
 
-(1,'Bureau étudiant blanc',40,'Bon état',
+('Bureau étudiant blanc',40,'Bon état',
 'Bureau compact parfait pour une chambre étudiante. Plusieurs rangements inclus.',
 'bureau.jpg'),
 
-(1,'Chaise de bureau',20,'Bon état',
+('Chaise de bureau',20,'Bon état',
 'Chaise confortable réglable en hauteur. Idéale pour travailler plusieurs heures.',
 'chaise.jpg'),
 
-(1,'Ordinateur Lenovo IdeaPad',220,'Très bon état',
+('Ordinateur Lenovo IdeaPad',220,'Très bon état',
 'Ordinateur portable Lenovo avec SSD rapide. Convient pour les cours et projets universitaires.',
 'lenovo.jpg'),
 
-(1,'Écran Samsung 24 pouces',60,'Très bon état',
+(‘Écran Samsung 24 pouces',60,'Très bon état',
 'Écran Full HD pour travailler plus confortablement sur ses projets.',
 'ecran.jpg'),
 
-(1,'Livre Algorithmique',10,'Bon état',
+('Livre Algorithmique',10,'Bon état',
 'Livre d algorithmique utilisé en première année informatique.',
 'algorithme.jpg'),
 
-(1,'Vélo étudiant',80,'Bon état',
+('Vélo étudiant',80,'Bon état',
 'Vélo pratique pour se déplacer entre le logement et l université.',
-'velo.jpg'),
+'velo.jpg’),
+('Air Fryer Philips', 45, 'Très bon état',
+'Utilisée pendant un semestre. Idéale pour cuisiner rapidement dans un studio étudiant.',
+'airfryer.jpg'),
+('Roman La Femme de ménage', 10, 'Très bon état',
+'Roman à succès de Freida McFadden. Lu une seule fois, état impeccable.',
+'femmedemenage.jpg'),
 
-(1,'Sac à dos Eastpak',15,'Bon état',
-'Sac à dos solide avec plusieurs compartiments pour ordinateur et cours.',
-'sac.jpg'),
+('Livre Atomic Habits', 12, 'Très bon état',
+'Livre sur les habitudes et l organisation. Très utile pour les étudiants.',
+'atomichabits.jpg'),
 
-(1,'Imprimante HP',30,'Bon état',
-'Imprimante couleur fonctionnelle. Fournie avec câble alimentation.',
-'imprimante.jpg'),
-
-(1,'Lampe de bureau LED',15,'Comme neuf',
-'Lampe LED économique avec plusieurs niveaux de luminosité.',
-'lampe.jpg'),
-
-(1,'Clavier Logitech',18,'Très bon état',
-'Clavier USB silencieux adapté aux études et au télétravail.',
-'clavier.jpg'),
-
-(1,'Souris sans fil Logitech',12,'Très bon état',
-'Souris sans fil fonctionnant parfaitement avec pile incluse.',
-'souris.jpg'),
-
-(1,'Tablette Samsung Galaxy Tab',95,'Bon état',
-'Tablette idéale pour prendre des notes en cours et consulter des PDF.',
-'tablette.jpg'),
-
-(1,'Casque Bluetooth JBL',35,'Très bon état',
-'Casque audio avec bonne autonomie et qualité sonore.',
-'casque.jpg'),
-  (1,'Power Bank Xiaomi',18,'Très bon état',
-'Batterie externe 10000 mAh idéale pour recharger téléphone et tablette pendant les cours.',
-'powerbank.jpg'),
-
-(1,'Support ordinateur portable',12,'Comme neuf',
-'Support réglable permettant de travailler plus confortablement.',
-'supportpc.jpg'),
-
-(1,'Câble HDMI',5,'Neuf',
-'Câble HDMI de 2 mètres pour connecter ordinateur et écran.',
-'hdmi.jpg'),
-
-(1,'Écouteurs Bluetooth',20,'Très bon état',
-'Écouteurs sans fil avec boîtier de recharge.',
-'ecouteurs.jpg'),
-
-(1,'Livre Python pour débutants',15,'Bon état',
-'Livre d initiation à la programmation Python.',
+('Livre Python pour débutants', 15, 'Très bon état',
+'Livre clair et facile à comprendre pour apprendre Python.',
 'python.jpg'),
 
-(1,'Livre Développement Web',14,'Bon état',
-'Cours complet HTML, CSS et PHP.',
-'web.jpg’);
+('Pack de cahiers universitaires', 5, 'Comme neuf',
+'Lot de 5 cahiers grands formats presque jamais utilisés.',
+'cahiers.jpg'),
 
-(1,'Mini réfrigérateur',70,'Bon état',
-'Petit frigo parfait pour un studio étudiant.',
-'frigo.jpg'),
+('Trousse complète', 4, 'Bon état',
+'Trousse avec stylos, surligneurs et crayons.',
+'trousse.jpg'),
 
-(1,'Micro-ondes',25,'Bon état',
-'Micro-ondes fonctionnel pour équiper un logement étudiant.',
-'microondes.jpg'),
+('Imprimante Canon', 35, 'Bon état',
+'Imprimante fonctionnelle. Vendue avec câble d alimentation.',
+'imprimante.jpg'),
 
-(1,'Bibliothèque IKEA',45,'Bon état',
-'Meuble de rangement pratique pour livres et cours.',
+('Tablette Samsung Galaxy Tab', 90, 'Bon état',
+'Pratique pour prendre des notes en cours et lire les PDF.',
+'tablette.jpg'),
+
+('Bibliothèque IKEA blanche', 30, 'Bon état',
+'Bibliothèque idéale pour ranger les cours et les romans.',
 'bibliotheque.jpg'),
 
-(1,'Livre SQL et Bases de données',12,'Très bon état',
-'Livre d introduction aux bases de données relationnelles et SQL.',
-'sql.jpg'),
+('Roman Normal People', 9, 'Bon état',
+'Roman contemporain très apprécié des étudiants.',
+'normalpeople.jpg'),
 
-(1,'Webcam HD',20,'Comme neuf',
-'Webcam Full HD pour cours à distance et réunions.',
-'webcam.jpg'),
+('Calculatrice Casio FX92', 10, 'Très bon état',
+'Calculatrice scientifique utilisée en lycée et université.',
+'fx92.jpg');
+('Machine à café Nespresso', 30, 'Bon état',
+'Fonctionne parfaitement. Vendue avec quelques capsules.',
+'nespresso.jpg'),
 
-(1,'Disque SSD 500 Go',40,'Très bon état',
-'SSD rapide idéal pour améliorer les performances d un ordinateur portable.',
-'ssd.jpg');
+('Trottinette Xiaomi', 120, 'Bon état',
+'Pratique pour aller en cours. Batterie en bon état.',
+'trottinette.jpg'),
+
+('Casque Sony Bluetooth', 55, 'Très bon état',
+'Bonne qualité sonore pour écouter de la musique ou suivre les cours en ligne.',
+'casquesony.jpg'),
+
+('Enceinte Bluetooth JBL', 28, 'Bon état',
+'Petite enceinte avec un bon son et une bonne autonomie.',
+'jbl.jpg'),
+
+('Sac Eastpak noir', 18, 'Bon état',
+'Sac solide avec compartiment pour ordinateur portable.',
+'eastpak.jpg'),
+
+('Lampe de chevet LED', 10, 'Comme neuf',
+'Lampe moderne avec intensité réglable.',
+'lampeled.jpg'),
+
+('Mini projecteur vidéo', 65, 'Bon état',
+'Pratique pour regarder des films entre amis dans une chambre étudiante.',
+'projecteur.jpg'),
+
+('Plaid polaire', 8, 'Comme neuf',
+'Très confortable pour les soirées d hiver.',
+'plaid.jpg’);
