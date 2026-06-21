639b1168c764e43b92a41e025767561c1915da89
sarahworld-ai <zemourisarah54@gmail.com>
2026-06-21 19:57:45 +0200

    Create insert_into_utilisateurs.sql

---
+++ insert_into_utilisateurs.sql
@@ -0,0 +1,6 @@
+NSERT INTO utilisateurs (id, nom, email, mot_de_passe, role) VALUES
+(1, 'Sarah', 'sarah@test.com', '123456', 'membre'),
+(2, 'Mellina', 'mellina@test.com', '123456', 'membre'),
+(3, 'Adam', 'adam@test.com', '123456', 'membre'),
+(4, 'Lina', 'lina@test.com', '123456', 'membre'),
+(5, 'Yassine', 'yassine@test.com', '123456', 'membre');
