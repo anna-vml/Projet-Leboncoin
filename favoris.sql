774371d8de25c379486d1b7b6611fd3faf90e23b
mellitaroudjit24 <melli.taroudjit24@gmail.com>
2026-06-24 07:24:49 -0400

    Update favoris.sql

--- favoris.sql
+++ favoris.sql
@@ -1,7 +1,11 @@
 CREATE TABLE favoris (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    id_utilisateur INT NOT NULL,
-    id_annonce INT NOT NULL,
-    FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_annonce) REFERENCES annonces(id) ON DELETE CASCADE
+    utilisateur_id INT NOT NULL,
+    annonce_id INT NOT NULL,
+    date_ajout DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs(id) ON DELETE CASCADE,
+    FOREIGN KEY (annonce_id) REFERENCES annonces(id) ON DELETE CASCADE,
+
+    UNIQUE (utilisateur_id, annonce_id)
 );
